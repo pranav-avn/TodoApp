e155a5fbd648280f58c47e2fc4b8e8858130f814
Pranav AVN <92722256+pranav-avn@users.noreply.github.com>
2021-12-21 21:52:04 +0530

    Updated main.py
    Formatted the code to signify that this is the py file to run to invoke the app

--- Todo App/main.py
+++ Todo App/main.py
@@ -1,8 +1,9 @@
 import eel
 import logic.todo_controller
 
+def main():
+  eel.init('web') 
+  eel.start('index.html', size=(650, 612), position=(0,0))
 
-eel.init('web') 
-
-
-eel.start('index.html', size=(650, 612), position=(0,0))
+if __name__=="__main__":
+    main()
